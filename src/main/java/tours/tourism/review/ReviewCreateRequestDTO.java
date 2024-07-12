58cf8fe5963415b15b18507c0fa6964c702a6036
DO YOUNG KIM <dlsrksrlaehdud@naver.com>
2024-07-12 19:37:39 +0900

    validation 추가

--- src/main/java/tours/tourism/review/ReviewCreateRequestDTO.java
+++ src/main/java/tours/tourism/review/ReviewCreateRequestDTO.java
@@ -1,11 +1,11 @@
 package tours.tourism.review;
 
+import jakarta.validation.constraints.NotNull;
 import tours.tourism.place.Place;
 
 public record ReviewCreateRequestDTO(
-        Double rating,
-        String comment,
-        Long placeId,
-        Long userId
+        @NotNull Double rating,
+        @NotNull String comment,
+        @NotNull Long userId
 ) {
 }
