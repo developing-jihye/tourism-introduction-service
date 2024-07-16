e2c298a937a398efc4e826bfc8a39d4284f6496c
LSH <iks2122@gmail.com>
2024-07-16 15:01:26 +0900

    PlaceDetailResponseDTO 생성

---
+++ src/main/java/tours/tourism/place/PlaceDetailResponseDTO.java
@@ -0,0 +1,34 @@
+package tours.tourism.place;
+
+import jakarta.validation.constraints.NotNull;
+import tours.tourism.review.Review;
+
+import java.util.List;
+
+public record PlaceDetailResponseDTO(
+        @NotNull
+        String name,
+
+        String imageUrl,
+
+        Double rating,
+
+        String address,
+
+        String time,
+
+        String description,
+
+        String phoneNumber,
+
+        @NotNull
+        City city,
+
+        @NotNull
+        Category category,
+
+        String website,
+
+        List<Review> reviews
+){
+}
