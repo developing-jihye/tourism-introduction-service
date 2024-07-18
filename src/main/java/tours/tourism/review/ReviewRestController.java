84687a4dbbee7c42324f77dc4a580528bf0b4a21
DO YOUNG KIM <dlsrksrlaehdud@naver.com>
2024-07-18 20:35:26 +0900

    컨트롤러명 변경

--- src/main/java/tours/tourism/review/ReviewRestController.java
+++ src/main/java/tours/tourism/review/ReviewRestController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/review")
-public class ReviewController {
+public class ReviewRestController {
 
     private final ReviewService reviewService;
 
-    public ReviewController(ReviewService reviewService) {
+    public ReviewRestController(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
 
