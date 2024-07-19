90b63b84b77bfa9786bc7964f869cae0dd3d9941
hee <cyh2616@naver.com>
2024-07-19 15:35:51 +0900

    로그인 - 설정 추가

---
+++ src/main/java/tours/tourism/LoginFailedException.java
@@ -0,0 +1,11 @@
+package tours.tourism;
+
+public class LoginFailedException extends RuntimeException {
+    public LoginFailedException() {
+        super("로그인 정보가 유효하지 않습니다");
+    }
+
+    public LoginFailedException(String message) {
+        super(message);
+    }
+}
